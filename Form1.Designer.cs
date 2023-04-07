865abf81205c001c81b879d6baea5923f6931f36
Guy Siedes <3strategy@gmail.com>
2023-04-07 13:10:29 +0300

    captures key but not filtering on key modifiers...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,32 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      buttonHook = new Button();
+      SuspendLayout();
+      // 
+      // buttonHook
+      // 
+      buttonHook.Location = new Point(715, 204);
+      buttonHook.Name = "buttonHook";
+      buttonHook.Size = new Size(75, 23);
+      buttonHook.TabIndex = 0;
+      buttonHook.Text = "buttonHook";
+      buttonHook.UseVisualStyleBackColor = true;
+      buttonHook.Click += buttonHook_Click;
+      // 
+      // Form1
+      // 
+      AutoScaleDimensions = new SizeF(7F, 15F);
+      AutoScaleMode = AutoScaleMode.Font;
+      ClientSize = new Size(800, 450);
+      Controls.Add(buttonHook);
+      Name = "Form1";
+      Text = "Form1";
+      ResumeLayout(false);
     }
 
     #endregion
+
+    private Button buttonHook;
   }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,33 @@ namespace KeboardHookAndCodeToHtml
     {
       InitializeComponent();
     }
+
+    private GlobalKeyboardHook _globalKeyboardHook;
+
+    private void buttonHook_Click(object sender, EventArgs e)
+    {
+      buttonHook.Enabled = false;
+      // Hooks only into specified Keys (here "A" and "B").
+      _globalKeyboardHook = new GlobalKeyboardHook(new Keys[] { Keys.C, Keys.B });
+
+      // Hooks into all keys.
+      //_globalKeyboardHook = new GlobalKeyboardHook();
+      _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+    }
+
+    private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+    {
+      // EDT: No need to filter for VkSnapshot anymore. This now gets handled
+      // through the constructor of GlobalKeyboardHook(...).
+      if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
+      {
+        // Now you can access both, the key and virtual code
+        Keys loggedKey = e.KeyboardData.Key;
+        if (loggedKey == Keys.C && e.KeyboardData.Flags==16) 
+        {
+        }
+        int loggedVkCode = e.KeyboardData.VirtualCode;
+      }
+    }
   }
 }
