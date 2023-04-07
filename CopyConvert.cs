59a574b62c6b6abf566a5220ce9e1aae2b27124d
Guy Siedes <3strategy@gmail.com>
2023-04-07 15:55:45 +0300

    Keyboard hook works ok on Ctrl+Shift+C using nuget package.

---
+++ CopyConvert.cs
@@ -0,0 +1,42 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeboardHookAndCodeToHtml
+{
+  public partial class CopyConvert : Form
+  {
+    public CopyConvert()
+    {
+      InitializeComponent();
+      //1. Define key combinations
+      var fullScrCombi = Combination.FromString("Control+Shift+F");
+      var copyConvertPasteCombi = Combination.FromString("Control+Shift+C");
+
+      //2. Define actions
+      Action actionConvert = CopyConvertToHtml;
+      Action actionFullScreen = () => { Console.WriteLine("You Pressed FULL SCREEN"); };
+
+      //3. Assign actions to key combinations
+      var assignment = new Dictionary<Combination, Action>
+      {
+          {fullScrCombi, actionFullScreen},
+          {copyConvertPasteCombi, actionConvert}
+      };
+
+      //4. Install listener
+      Hook.GlobalEvents().OnCombination(assignment);
+    }
+    void CopyConvertToHtml()
+    {
+      Console.WriteLine("You pressed Convert");
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ namespace KeboardHookAndCodeToHtml
     {
       // To customize application configuration such as set high DPI settings or default font,
       // see https://aka.ms/applicationconfiguration.
-      ApplicationConfiguration.Initialize();
-      Application.Run(new Form1());
+      ApplicationConfiguration.Initialize(); 
+      Application.SetCompatibleTextRenderingDefault(false);
+      //Application.Run(new Form1());
+      //Application.Run(new Form2());
+      Application.Run(new CopyConvert());
+
     }
   }
 }
