9d66755f010815fcc3161a1ee90d13e189e98865
Guy Siedes <3strategy@gmail.com>
2023-05-23 08:05:13 +0300

    assign answers to swiftAns class

--- CopyConvert.Designer.cs
+++ CopyConvert.Designer.cs
@@ -1,61 +1,74 @@
 ﻿namespace KeboardHookAndCodeToHtml
 {
-  partial class CopyConvert
-  {
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class CopyConvert
     {
-      if (disposing && (components != null))
-      {
-        components.Dispose();
-      }
-      base.Dispose(disposing);
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBoxLineNumbers = new CheckBox();
+            chkSwiftAns = new CheckBox();
+            SuspendLayout();
+            // 
+            // checkBoxLineNumbers
+            // 
+            checkBoxLineNumbers.AutoSize = true;
+            checkBoxLineNumbers.Checked = true;
+            checkBoxLineNumbers.CheckState = CheckState.Checked;
+            checkBoxLineNumbers.Location = new Point(20, 225);
+            checkBoxLineNumbers.Name = "checkBoxLineNumbers";
+            checkBoxLineNumbers.Size = new Size(125, 19);
+            checkBoxLineNumbers.TabIndex = 0;
+            checkBoxLineNumbers.Text = "Add Line Numbers";
+            checkBoxLineNumbers.UseVisualStyleBackColor = true;
+            // 
+            // chkSwiftAns
+            // 
+            chkSwiftAns.AutoSize = true;
+            chkSwiftAns.Location = new Point(364, 223);
+            chkSwiftAns.Name = "chkSwiftAns";
+            chkSwiftAns.Size = new Size(118, 19);
+            chkSwiftAns.TabIndex = 1;
+            chkSwiftAns.Text = "format as Answer";
+            chkSwiftAns.UseVisualStyleBackColor = true;
+            // 
+            // CopyConvert
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 498);
+            Controls.Add(chkSwiftAns);
+            Controls.Add(checkBoxLineNumbers);
+            Name = "CopyConvert";
+            Text = "Clipboard to Html";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private CheckBox checkBoxLineNumbers;
+        private CheckBox chkSwiftAns;
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-      checkBoxLineNumbers = new CheckBox();
-      SuspendLayout();
-      // 
-      // checkBoxLineNumbers
-      // 
-      checkBoxLineNumbers.AutoSize = true;
-      checkBoxLineNumbers.Checked = true;
-      checkBoxLineNumbers.CheckState = CheckState.Checked;
-      checkBoxLineNumbers.Location = new Point(20, 225);
-      checkBoxLineNumbers.Name = "checkBoxLineNumbers";
-      checkBoxLineNumbers.Size = new Size(125, 19);
-      checkBoxLineNumbers.TabIndex = 0;
-      checkBoxLineNumbers.Text = "Add Line Numbers";
-      checkBoxLineNumbers.UseVisualStyleBackColor = true;
-      // 
-      // CopyConvert
-      // 
-      AutoScaleDimensions = new SizeF(7F, 15F);
-      AutoScaleMode = AutoScaleMode.Font;
-      ClientSize = new Size(800, 498);
-      Controls.Add(checkBoxLineNumbers);
-      Name = "CopyConvert";
-      Text = "Clipboard to Html";
-      ResumeLayout(false);
-      PerformLayout();
-    }
-
-    #endregion
-    private CheckBox checkBoxLineNumbers;
-  }
 }
--- CopyConvert.cs
+++ CopyConvert.cs
@@ -4,74 +4,75 @@ using CsharpToColouredHTML.Core;
 
 namespace KeboardHookAndCodeToHtml
 {
-  public partial class CopyConvert : Form
-  {
-    private TextBox originalText; // These texts can be ignored. The app should normally be used without the GUI.
-    private TextBox convertedText;
-    public CopyConvert()
+    public partial class CopyConvert : Form
     {
-      // create converted text box
-      convertedText = new TextBox();
-      convertedText.Dock = DockStyle.Bottom;
-      convertedText.Multiline = true;
-      convertedText.Height = 200;
-      convertedText.ScrollBars = ScrollBars.Vertical;
-      //convertedText.ReadOnly = true;
-      Controls.Add(convertedText);
+        private TextBox originalText; // These texts can be ignored. The app should normally be used without the GUI.
+        private TextBox convertedText;
+        public CopyConvert()
+        {
+            // create converted text box
+            convertedText = new TextBox();
+            convertedText.Dock = DockStyle.Bottom;
+            convertedText.Multiline = true;
+            convertedText.Height = 200;
+            convertedText.ScrollBars = ScrollBars.Vertical;
+            //convertedText.ReadOnly = true;
+            Controls.Add(convertedText);
 
-      // create original text box
-      originalText = new TextBox();
-      originalText.Dock = DockStyle.Top;
-      originalText.Multiline = true;
-      originalText.Height = 200;
-      originalText.ScrollBars = ScrollBars.Vertical;
-      originalText.Text = "To use this just copy the code you need (Ctrl+C), then press Ctrl+Shift+C to convert." +
-        "\r\nThe original clipboard code will appear here, and the HTML code will appear below" +
-        "\r\nThe HTML is automatically copied to the clipboard\r\n\r\n" +
-        "You may use the conversion shortcut while this app is in the background.\r\nFeel free to minimize this window";
-      Controls.Add(originalText);
+            // create original text box
+            originalText = new TextBox();
+            originalText.Dock = DockStyle.Top;
+            originalText.Multiline = true;
+            originalText.Height = 200;
+            originalText.ScrollBars = ScrollBars.Vertical;
+            originalText.Text = "To use this just copy the code you need (Ctrl+C), then press Ctrl+Shift+C to convert." +
+              "\r\nThe original clipboard code will appear here, and the HTML code will appear below" +
+              "\r\nThe HTML is automatically copied to the clipboard\r\n\r\n" +
+              "You may use the conversion shortcut while this app is in the background.\r\nFeel free to minimize this window";
+            Controls.Add(originalText);
 
-      InitializeComponent();
-      //1. Define key combinations
-      var fullScrCombi = Combination.FromString("Control+Shift+F");
-      var copyConvertPasteCombi = Combination.FromString("Control+Shift+C");
-      var copyConvertPasteCombi2 = Combination.FromString("Control+Shift+X");
+            InitializeComponent();
+            //1. Define key combinations
+            var fullScrCombi = Combination.FromString("Control+Shift+F");
+            var copyConvertPasteCombi = Combination.FromString("Control+Shift+C");
+            var copyConvertPasteCombi2 = Combination.FromString("Control+Shift+X");
 
-      //2. Define actions
-      Action actionConvert = CopyConvertToHtml;
-      Action actionFullScreen = () => { Console.WriteLine("You Pressed FULL SCREEN"); };
+            //2. Define actions
+            Action actionConvert = CopyConvertToHtml;
+            Action actionFullScreen = () => { Console.WriteLine("You Pressed FULL SCREEN"); };
 
-      //3. Assign actions to key combinations
-      var assignment = new Dictionary<Combination, Action>
+            //3. Assign actions to key combinations
+            var assignment = new Dictionary<Combination, Action>
       {
           {fullScrCombi, actionFullScreen},
           {copyConvertPasteCombi, actionConvert},
           {copyConvertPasteCombi2, actionConvert}
       };
 
-      //4. Install listener
-      Hook.GlobalEvents().OnCombination(assignment);
-    }
-    void CopyConvertToHtml()
-    {
-      originalText.Text = Clipboard.GetText(); //get clipboard content
-      HTMLEmitterSettings settings;
-      if (checkBoxLineNumbers.Checked)
-        settings = new HTMLEmitterSettings().DisableIframe();
-      else
-        settings = new HTMLEmitterSettings().DisableIframe().DisableLineNumbers();
-      
-      //This was not recently tested without the line numbers
-      string convertedHtml = new CsharpColourer().ProcessSourceCode(originalText.Text, new HTMLEmitter(settings));
-      //update the converted html to include some relevant tags to make it work
-      //properly inside campus site.
-      int i = convertedHtml.IndexOf("<pre class=\"background\"") + 24;
-      string embeddedHtml = "<div class=\"code swiftly\"><pre>" + convertedHtml.Substring(i, convertedHtml.Length - i);
-      i = embeddedHtml.IndexOf("</pre>");
-      embeddedHtml = embeddedHtml.Substring(0, i) + "</pre></div>";
+            //4. Install listener
+            Hook.GlobalEvents().OnCombination(assignment);
+        }
+        void CopyConvertToHtml()
+        {
+            originalText.Text = Clipboard.GetText(); //get clipboard content
+            HTMLEmitterSettings settings;
+            if (checkBoxLineNumbers.Checked)
+                settings = new HTMLEmitterSettings().DisableIframe();
+            else
+                settings = new HTMLEmitterSettings().DisableIframe().DisableLineNumbers();
 
-      convertedText.Text = embeddedHtml;
-      Clipboard.SetText(embeddedHtml); //save the conversion result back to the clipboard
+            //This was not recently tested without the line numbers
+            string convertedHtml = new CsharpColourer().ProcessSourceCode(originalText.Text, new HTMLEmitter(settings));
+            //update the converted html to include some relevant tags to make it work
+            //properly inside campus site.
+            int i = convertedHtml.IndexOf("<pre class=\"background\"") + 24;
+            string swift = chkSwiftAns.Checked ? "swiftAns" : "swiftly"; //קביעת המחלקה בהתאם לסוף התוצר
+            string embeddedHtml = "<div class=\"code "+ swift + "\"><pre>" + convertedHtml.Substring(i, convertedHtml.Length - i);
+            i = embeddedHtml.IndexOf("</pre>");
+            embeddedHtml = embeddedHtml.Substring(0, i) + "</pre></div>";
+
+            convertedText.Text = embeddedHtml;
+            Clipboard.SetText(embeddedHtml); //save the conversion result back to the clipboard
+        }
     }
-  }
 }
